c9c38d8f633bdc2e57f99d2f2c18395c97a2175c
ullas <ullaskunder3@gmail.com>
2021-12-17 00:14:26 +0530

    setting up ejs and testing

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res)=>{
-    res.send('testing end point...')
+    res.redirect('/landingpage')
+})
+app.get('/landingpage', (req,res)=>{
+    res.render('landingpage')
+})
+app.get('/login', (req,res)=>{
+    res.render('login')
+})
+app.get('/register', (req,res)=>{
+    res.render('register')
 })
 
 app.listen(5050, ()=>{
