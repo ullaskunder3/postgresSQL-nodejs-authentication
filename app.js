1f91daa0f6a13baf088a14d6e52e892df63a7ab0
ullas <ullaskunder3@gmail.com>
2021-12-28 12:43:30 +0530

    db_config, fix layout, validation

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
+const bycryt = require('bcrypt');
 const app = express();
+const {pool} = require('./db_config');
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.use(express.static(__dirname+'/'))
 
 app.get('/', (req,res)=>{
-    res.redirect('/landingpage')
+    res.redirect('/main')
 })
-app.get('/users/landingpage', (req,res)=>{
-    res.render('landingpage')
+app.get('/main', (req,res)=>{
+    res.render('main')
 })
 app.get('/users/register', (req,res)=>{
     res.render('register')
@@ -17,8 +20,38 @@ app.get('/users/register', (req,res)=>{
 app.get('/users/login', (req,res)=>{
     res.render('login')
 })
-app.get('/users/user', (req,res)=>{
-    res.render('user')
+app.get('/users/profile', (req,res)=>{
+    res.render('profile', {user: 'ullas'})
+})
+app.post('/users/register', async (req, res)=>{
+    let {name, email, password, password2} = req.body;
+    console.log({
+        name, password, email 
+    });
+    let errors = [];
+    if(!name || !email || !password || !password2){
+        errors.push({message: 'please enter all the fields'})
+    }
+    if(password.length < 5){
+        errors.push({message: 'password should atlest 6 character long '})
+    }
+    if(password !== password2){
+        errors.push({message: 'password do not match'})
+    }
+    if(errors.length > 0){
+        res.render('register', {errors})
+    }else{
+        let hashedPass = await bycryt.hash(password, 10);
+
+        pool.query(
+            `SELECT * FROM users
+            WHERE email = $1,`[email], (err, results)=>{
+                if(err){
+                    throw err
+                }
+            }
+        )
+    }
 })
 
 app.listen(5050, ()=>{
---
+++ db_config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const {Pool} = require('pg');
+const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@{process.env.DB_HOST}:${process.env.DB_POST}/${process.env.DB_DATABASE}`
+const pool = new Pool({
+    connectionString: isProduction?process.env.DB_DATABASE_URL:connectionString
+})
+module.exports = {pool};
